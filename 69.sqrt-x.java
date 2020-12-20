cfc7f5a53c8f24092962931486794c209c50a28c
shannon <shannon.gu1031@gmail.com>
2020-12-20 18:27:32 +0800

    update solution to 69

--- 69.sqrt-x.java
+++ 69.sqrt-x.java
@@ -4,37 +4,25 @@
  * [69] Sqrt(x)
  */
 class Solution {
-    //https://www.cnblogs.com/grandyang/p/4346413.html
-    //https://www.acwing.com/solution/LeetCode/content/154/
-    // public int mySqrt(int x) {
-    //     if (x <= 1)
-    //         return x;
-    //     long l = 0, r = x;
-    //     while (l < r) {
-    //         int mid = l + ((r - l) >> 1);
-    //         if (x / mid >= mid)
-    //             l = mid + 1;
-    //         else
-    //             r = mid;
-    //     }
-    //     return l - 1;
-
-    // }
-
+    // https://www.cnblogs.com/grandyang/p/4346413.html
+    // https://www.acwing.com/solution/LeetCode/content/154/
+    // 二分法
+    // 如果将区间划分为[l, m]和[m+1, r]
+    // 则mid=l+r>>1,相应地l=m+1或者r=m
+    // 如果将区间划分为[l, m-1]和[m, r]
+    // 则mid=l+r+1>11,相应地l=m或者r=m-1
     public int mySqrt(int x) {
-        if(x < 0)
-            return -1;
-        if(x <= 1)
+        if (x <= 1)
             return x;
         long l = 0, r = x;
         while (l < r) {
             long mid = l + r + 1 >> 1;
+            // 在[m, r]范围内
             if (x / mid >= mid)
                 l = mid;
-            else
+            else // 在[l, m-1]范围内
                 r = mid - 1;
         }
         return (int) l;
     }
 }
-
