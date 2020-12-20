5495c94d5a4434d32411e35e4a2d2fbbd4e7b52c
shannon <shannon.gu1031@gmail.com>
2020-12-20 19:39:01 +0800

    update 153

--- 153.find-minimum-in-rotated-sorted-array.java
+++ 153.find-minimum-in-rotated-sorted-array.java
@@ -4,38 +4,15 @@
  * [153] Find Minimum in Rotated Sorted Array
  */
 class Solution {
-    //https://www.cnblogs.com/grandyang/p/4032934.html
-    // public int findMin(int[] nums) {
-    //     int l = 0, r = nums.length - 1;
-    //     int pos = 0;
-    //     while (l < r) {
-    //         int mid = l + ((r - l) >> 1);
-    //         //右半部分有序
-    //         if (nums[mid] < nums[r]) {
-    //             if (mid == 0 || nums[mid - 1] > nums[mid]) {
-    //                 pos = mid;
-    //                 break;
-    //             } else
-    //                 r = mid;
-    //         } else {
-    //             //左半部分有序
-    //             if (nums[mid] > nums[mid + 1]) {
-    //                 pos = mid + 1;
-    //                 break;
-    //             } else
-    //                 l = mid + 1;
-    //         }
-    //     }
-    //     return nums[pos];
-    // }
-
 
     public int findMin(int[] nums) {
         int l = 0, r = nums.length - 1;
-        //先判断是不是整体有序
+        // 先判断是否整体有序
         if (nums[l] > nums[r]) {
             while (l < r) {
                 int mid = l + r >> 1;
+                // [mid+1, r]部分有序，答案在[l, mid]中,
+                // 划分为[l, mid]和[mid+1, r]
                 if (nums[mid] < nums[r])
                     r = mid;
                 else
@@ -46,4 +23,3 @@ class Solution {
         return nums[0];
     }
 }
-
