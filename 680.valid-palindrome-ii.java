67ef4594256fba7724d1ea971a7998e06e3b257d
shannon <shannon.gu1031@gmail.com>
2020-12-20 16:42:46 +0800

    add 680

---
+++ 680.valid-palindrome-ii.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=680 lang=java
+ *
+ * [680] Valid Palindrome II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return judge(s, l, r - 1) || judge(s, l + 1, r);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    private boolean judge(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+// @lc code=end
