584cbebd490716f0b44e2b3a101166a48f77cc38
shannon <shannon.gu1031@gmail.com>
2020-12-23 00:27:01 +0800

    update 206

--- 206.reverse-linked-list.java
+++ 206.reverse-linked-list.java
@@ -4,38 +4,46 @@
  * [206] Reverse Linked List
  */
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
  */
 class Solution {
+    // 头插法
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = null;
+        ListNode p = head;
+        while (p != null) {
+            ListNode q = p.next;
+            p.next = dummy.next;
+            dummy.next = p;
+            p = q;
+        }
+        return dummy.next;
+    }
+
     // public ListNode reverseList(ListNode head) {
-    //     if(head == null || head.next == null)
-    //         return head;
-    //     ListNode dummy = new ListNode(-1);
-    //     dummy.next = null;
-    //     ListNode p = head;
-    //     while (p != null) {
-    //         ListNode q = p.next;
-    //         p.next = dummy.next;
-    //         dummy.next = p;
-    //         p = q;
-    //     }
-    //     return dummy.next;
+    // ListNode prev = null, next = null;
+    // while (head != null) {
+    // next = head.next;
+    // head.next = prev;
+    // prev = head;
+    // head = next;
+    // }
+    // return prev;
     // }
 
-    public ListNode reverseList(ListNode head) {
-        //空链表或者只有一个元素
-        //直接返回
-        if(head == null || head.next == null)
-            return head;
-        ListNode p = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return p;
-    }
+    // 递归
+    // public ListNode reverseList(ListNode head) {
+    // //空链表或者只有一个元素
+    // //直接返回
+    // if (head == null || head.next == null)
+    // return head;
+    // ListNode newHead = reverseList(head.next);
+    // head.next.next = head;
+    // head.next = null;
+    // return newHead;
+    // }
 }
-
