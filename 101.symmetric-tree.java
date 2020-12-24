ccc9b1e8fdf11a205e0be70d23aabe53ad7b2805
shannon <shannon.gu1031@gmail.com>
2020-12-24 23:44:51 +0800

    update 101

--- 101.symmetric-tree.java
+++ 101.symmetric-tree.java
@@ -3,28 +3,25 @@
  *
  * [101] Symmetric Tree
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         return judge(root, root);
     }
 
+    // 四步法
+    // (1)如果两个子树都为空指针，则它们相等或对称
+    // (2)如果两个子树只有一个为空指针，则它们不相等或不对称
+    // (3)如果两个子树根节点的值不相等， 则它们不相等或不对称
+    // (4)根据相等或对称要求，进行递归处理。
     public boolean judge(TreeNode p, TreeNode q) {
-        if(p == null && q ==null)
+        // 同时为空
+        if (p == null && q == null)
             return true;
-        if(p == null || q == null)
+        // 不同时为空
+        if (p == null || q == null)
             return false;
-        if(p.val != q.val)
+        if (p.val != q.val)
             return false;
         return judge(p.left, q.right) && judge(p.right, q.left);
     }
 }
-
