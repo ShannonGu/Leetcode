1c97d1aebff14e30ab74baad169ddf1fc6f7390c
shannon <shannon.gu1031@gmail.com>
2020-12-24 23:26:40 +0800

    543

--- 543.diameter-of-binary-tree.java
+++ 543.diameter-of-binary-tree.java
@@ -1,37 +1,28 @@
+import java.util.Map;
+
 /*
  * @lc app=leetcode id=543 lang=java
  *
  * [543] Diameter of Binary Tree
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         res = 0;
-        map = new HashMap<>();
         height(root);
         return res;
     }
 
+    private int res;
+
     private int height(TreeNode root) {
-        if (root == null)
+        if (root == null) {
             return 0;
-        if (map.containsKey(root))
-            return map.get(root);
+        }
+
         int lH = height(root.left);
         int rH = height(root.right);
-        res = Math.max(res, lH + rH);
-        return (lH > rH ? lH : rH) + 1;
+        res = Math.max(lH + rH, res);
+
+        return 1 + (lH > rH ? lH : rH);
     }
-
-    private int res;
-    private Map<TreeNode, Integer> map;
 }
-
