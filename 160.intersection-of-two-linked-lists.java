dc45977a2ed1c20a6442236267250dd129e29bf8
shannon <shannon.gu1031@gmail.com>
2020-12-24 19:09:50 +0800

    update 160

--- 160.intersection-of-two-linked-lists.java
+++ 160.intersection-of-two-linked-lists.java
@@ -4,70 +4,23 @@
  * [160] Intersection of Two Linked Lists
  */
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; next = null; } }
  */
 public class Solution {
-    //https://www.cnblogs.com/grandyang/p/4128461.html
-    // public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    //     if(headA == null || headB == null)
-    //         return null;
-    //     if(headA == headB)
-    //         return headA;
-    //     int lenA = Len(headA), lenB = Len(headB);
-
-    //     int cnt = 0;
-    //     if (lenA < lenB) {
-    //         ListNode tmp = headA;
-    //         headA = headB;
-    //         headB = tmp;
-    //         cnt = lenB - lenA;
-    //     }
-    //     else
-    //         cnt = lenA - lenB;
-
-    //     ListNode p = headA;
-    //     while (cnt-- > 0 && p != null) {
-    //         p = p.next;
-    //     }
-
-    //     headA = p;
-    //     while (headA != null && headB != null) {
-    //         if (headA == headB)
-    //             return headA;
-    //         headA = headA.next;
-    //         headB = headB.next;
-    //     }
-    //     return null;
-    // }
-
-    // public int Len(ListNode a) {
-    //     int cnt = 0;
-    //     while (a != null) {
-    //         ++cnt;
-    //         a = a.next;
-    //     }
-    //     return cnt;
-    // }
-
-
+    // https://www.cnblogs.com/grandyang/p/4128461.html
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null)
+        if (headA == null || headB == null)
             return null;
-        ListNode a = headA, b = headB;
-        //当一个链表遍历到结尾空节点时，转向到另一个链表的头结点
-        while (a != b) {
-            a = (a != null) ? a.next : headB;
-            b = (b != null) ? b.next : headA;
+        ListNode l1 = headA, l2 = headB;
+        // 假设A的头节点到相交点的距离为a，B的头节点到相交点的距离为b
+        // 相交点到终点的距离为c
+        // 当一个链表遍历到结尾空节点时，转向到另一个链表的头结点
+        // 按照此种方式，如果两个链表长度不同，最终会在a+b+c次前进后同时叨叨
+        while (l1 != l2) {
+            l1 = l1 != null ? l1.next : headB;
+            l2 = l2 != null ? l2.next : headA;
         }
-        return a;
+        return l1;
     }
 }
-
