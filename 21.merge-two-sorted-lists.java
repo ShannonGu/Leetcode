b0f413a6e5a10aedf8e921c03751a40211e2d791
shannon <shannon.gu1031@gmail.com>
2020-12-24 18:40:43 +0800

    update 21

--- 21.merge-two-sorted-lists.java
+++ 21.merge-two-sorted-lists.java
@@ -8,29 +8,21 @@
  * next; ListNode(int x) { val = x; } }
  */
 class Solution {
-    // public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    // if(l1 == null)
-    // return l2;
-    // if(l2 == null)
-    // return l1;
-    // ListNode dummy = new ListNode(-1);
-    // ListNode pre = dummy;
-    // while (l1 != null && l2 != null) {
-    // if (l1.val > l2.val) {
-    // ListNode t = l1;
-    // l1 = l2;
-    // l2 = t;
-    // }
-    // pre.next = l1;
-    // pre = l1;
-    // l1 = l1.next;
-    // }
-    // if(l1 != null)
-    // pre.next = l1;
-    // else if(l2 != null)
-    // pre.next = l2;
-    // return dummy.next;
-    // }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), pre = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null)
