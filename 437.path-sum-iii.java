8bfcd798b498488cefad92d9c436d913baffeea7
shannon <shannon.gu1031@gmail.com>
2020-12-24 23:34:14 +0800

    update 437

--- 437.path-sum-iii.java
+++ 437.path-sum-iii.java
@@ -9,35 +9,25 @@ import javax.swing.tree.TreeNode;
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     // https://www.jiuzhang.com/solution/path-sum-iii
     public int pathSum(TreeNode root, int sum) {
-        if(root == null)
+        if (root == null)
             return 0;
         return helper(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
     }
-    
-    // 以当前点为起点，和为sum的路径个数
-    private int helper(TreeNode node, int sum) {
-        if(node == null)
+
+    // 以当前点为起点，计算和为sum的路径个数
+    private int helper(TreeNode root, int sum) {
+        if (root == null)
             return 0;
-        int res = 0;
-        if (sum == node.val) {
-            res += 1;
-        }
-        res += helper(node.left, sum - node.val);
-        res += helper(node.right, sum - node.val);
-        return res;
+        int cnt = 0;
+        if (root.val == sum)
+            cnt += 1;
+        // 递归遍历左右子树
+        cnt += helper(root.left, sum - root.val);
+        cnt += helper(root.right, sum - root.val);
+        return cnt;
     }
 }
 // @lc code=end
-
