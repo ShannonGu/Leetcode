e2e925397a403c062db7a070d7bd035f2b3b2445
shannon <shannon.gu1031@gmail.com>
2020-12-26 23:31:17 +0800

    update 46

--- 46.permutations.java
+++ 46.permutations.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /*
  * @lc app=leetcode id=46 lang=java
@@ -8,31 +9,35 @@ import java.util.List;
  */
 class Solution {
     // https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
-    List<List<Integer>> res;
+    private List<List<Integer>> res;
 
     public List<List<Integer>> permute(int[] nums) {
         res = new ArrayList<>();
-        if (nums.length == 0)
-            return res;
-        boolean[] visited = new boolean[nums.length];
-        helper(nums, new ArrayList<>(), visited);
+        helper(nums, 0);
         return res;
     }
 
-    private void helper(int[] nums, List<Integer> temp, boolean[] visited) {
-        if (temp.size() == nums.length) {
-            res.add(new ArrayList<>(temp));
+    private void helper(int[] nums, int cnt) {
+        if (cnt == nums.length - 1) {
+            // int[] 转 List<Intgeger>
+            // Arrays.stream(arr) 可以替换成IntStream.of(arr)
+            // 1.使用Arrays.stream将int[]转换成IntStream
+            // 2.使用IntStream中的boxed()装箱, 将IntStream转换成Stream<Integer>
+            // 3.使用Stream的collect()，将Stream<T>转换成List<T>，因此正是List<Integer>
+            res.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
             return;
         }
 
-        for (int i = 0; i < nums.length; ++i) {
-            if (visited[i])
-                continue;
-            visited[i] = true;
-            temp.add(nums[i]);
-            helper(nums, temp, visited);
-            temp.remove(temp.size() - 1);
-            visited[i] = false;
+        for (int i = cnt; i < nums.length; ++i) {
+            swap(nums, i, cnt); // 修改当前数状态
+            helper(nums, cnt + 1);
+            swap(nums, i, cnt); // 还原当前数状态
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
