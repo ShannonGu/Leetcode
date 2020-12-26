fd50c30189b7c92fc66076038518e521fa00a953
shannon <shannon.gu1031@gmail.com>
2020-12-26 23:42:03 +0800

    update 77

--- 77.combinations.java
+++ 77.combinations.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 /*
  * @lc app=leetcode id=77 lang=java
  *
@@ -12,20 +16,21 @@ class Solution {
         if (n == 0)
             return new ArrayList<>();
         res = new ArrayList<>();
-        helper(n, k, 1, new ArrayList<>());
+        helper(n, 0, 1, new int[k], k);
         return res;
     }
 
-    private void helper(int n, int k, int pos, List<Integer> out) {
-        if (k == 0) {
-            res.add(new ArrayList<>(out));
+    private void helper(int n, int cnt, int pos, int[] out, int k) {
+        // out数组用于临时存放每个组合
+        if (cnt == k) {
+            res.add(Arrays.stream(out).boxed().collect(Collectors.toList()));
             return;
         }
 
         for (int i = pos; i <= n; ++i) {
-            out.add(i);
-            helper(n, k - 1, i + 1, out);
-            out.remove(out.size() - 1);
+            out[cnt++] = i; // 修改当前数状态
+            helper(n, cnt, i + 1, out, k);
+            --cnt; // 还原当前数状态
         }
     }
 }
