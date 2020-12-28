ed7b6a694a09a5c8dcc2dbcea6d365c53a3a062f
shannon <shannon.gu1031@gmail.com>
2020-12-28 19:51:07 +0800

    update 47

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -1,36 +1,38 @@
+import java.util.List;
+
 /*
  * @lc app=leetcode id=47 lang=java
  *
  * [47] Permutations II
  */
 class Solution {
-    //https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
+    // https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
     private List<List<Integer>> res;
 
     public List<List<Integer>> permuteUnique(int[] nums) {
         res = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
+        List<Integer> out = new ArrayList<>();
         Arrays.sort(nums);
-        helper(nums, new ArrayList<>(), visited);
-        return new ArrayList<>(res);
+        helper(nums, 0, out, visited);
+        return res;
     }
-    
-    private void helper(int[] nums, List<Integer> temp, boolean[] visited) {
-        if (temp.size() == nums.length) {
-            res.add(new ArrayList<>(temp));
+
+    private void helper(int[] nums, int level, List<Integer> out, boolean[] visited) {
+        if (level == nums.length) {
+            res.add(new ArrayList<>(out));
             return;
         }
 
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0; i < nums.length; i++) {
+            // 去重
             if (visited[i] || i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])
                 continue;
             visited[i] = true;
-            temp.add(nums[i]);
-            helper(nums, temp, visited);
-            temp.remove(temp.size() - 1);
+            out.add(nums[i]);
+            helper(nums, level + 1, out, visited);
+            out.remove(out.size() - 1);
             visited[i] = false;
         }
     }
-
 }
-
