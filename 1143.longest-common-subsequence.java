0118d6abcde8f54b252a91cc8c46db24f4d7fcd9
shannon <shannon.gu1031@gmail.com>
2021-01-06 01:13:05 +0800

    update 1143

--- 1143.longest-common-subsequence.java
+++ 1143.longest-common-subsequence.java
@@ -6,13 +6,8 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length(), m = text2.length();
+        // dp[i][j]表示text1[0, i)与text2[0, j)中的最长子序列的长度
         int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; ++i) {
-            dp[i][0] = 0;
-        }
-        for (int i = 0; i <= m; ++i) {
-            dp[0][i] = 0;
-        }
 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= m; ++j) {
@@ -26,4 +21,3 @@ class Solution {
         return dp[n][m];
     }
 }
-
