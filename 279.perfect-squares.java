73085f426a414ceddcbf0c69186fb778842a6fe6
shannon <shannon.gu1031@gmail.com>
2021-01-05 23:36:12 +0800

    update 279

---
+++ 279.perfect-squares.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=279 lang=java
+ *
+ * [279] Perfect Squares
+ */
+
+// @lc code=start
+class Solution {
+    public int numSquares(int n) {
+        // dp[i]表示数字i最少可以由几个完全平方数相加构成
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
