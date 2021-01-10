cd6a6cf8427179a6989a72286737f62c8a09c1e2
shannon <shannon.gu1031@gmail.com>
2021-01-10 23:15:13 +0800

    update 309

--- 309.best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309.best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -7,22 +7,35 @@ class Solution {
 
     // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/discuss/108870/most-consistent-ways-of-dealing-with-the-series-of-stock-problems
     public int maxProfit(int[] prices) {
-        if(prices.length == 0)
-            return 0;
-        //两天前的不持有状态
-        int preT_ik0 = 0, T_ik0 = 0, T_ik1 = Integer.MIN_VALUE;
+        int n = prices.length;
+        int[] f1 = new int[n + 1], f2 = new int[n + 1], f3 = new int[n + 1];
+        f1[0] = f2[0] = 0;
+        f3[0] = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            f3[i] = Math.max(f3[i - 1], f2[i - 1] - prices[i - 1]);
+            f1[i] = f3[i - 1] + prices[i - 1];
+            f2[i] = Math.max(f2[i - 1], f1[i - 1]);
+        }
+
+        return Math.max(f1[n], f2[n]);
+    }
+
+    public int maxProfit(int[] prices) {
+        // T_i1表示当前是手中没有股票的第一天
+        // T_i2表示当前是手中没有股票的第二天
+        // T_i3表示当前手中有股票
+        int T_i1 = 0, T_i2 = 0, T_i3 = Integer.MIN_VALUE;
 
         for (int price : prices) {
-            //如果前一天卖出的话，不能在当前时间买入
-            int oldT_ik0 = T_ik0;
-
-            T_ik0 = Math.max(T_ik0, T_ik1 + price);
-            T_ik1 = Math.max(T_ik1, preT_ik0 - price);
-            
-            //将前一天的不持有状态作为作为下一轮的两天前的不持有状态
-            preT_ik0 = oldT_ik0;
+            int preT_i3 = T_i3, preT_i1 = T_i1;
+            // 手中有股票只能从原状态维持下来，或者由没有股票的第二天这种状态转化而来
+            T_i3 = Math.max(T_i3, T_i2 - price);
+            // 没有股票的第1天这种状态只能由手中有股票这种状态卖出后得到
+            T_i1 = preT_i3 + price;
+            // 没有股票的第2天只能从原状态维持，或者没有股票的第1天转化而来
+            T_i2 = Math.max(T_i2, preT_i1);
         }
-        return T_ik0;
+        // 最后比较两种当前手中没有股票的状态
+        return Math.max(T_i1, T_i2);
     }
 }
-
